43c7086ebc1a2bd24d60c67cb1d9b6573c88796d
bertrandong <bertrandongchangheng@gmail.com>
2024-02-09 02:03:56 +0800

    Update navbar for mobile view

--- frontend/src/components/main/banner.jsx
+++ frontend/src/components/main/banner.jsx
@@ -1,11 +1,24 @@
-import { Box, Button, Heading, Spacer, Flex, Text } from "@chakra-ui/react";
+import { Box, Button, Heading, Spacer, Flex, Text, VStack } from "@chakra-ui/react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Nav from "./nav";
+import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons'
 
 export default function Banner(props) {
-  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false)
+  // const navigate = useNavigate();
 
-  function handleLogout() {
+  const toggle = () => setIsOpen(!isOpen)
+
+  const MenuToggle = ({ toggle, isOpen }) => {
+    return (
+      <Box fontSize='xl' pr={16} display={{ base: "block", md: "none" }} onClick={toggle}>
+        {isOpen ? <CloseIcon /> : <HamburgerIcon />}
+      </Box>
+    )
+  }
+
+  /**function handleLogout() {
     props.toast({
       title: "Successfully logged out.",
       description: "",
@@ -17,50 +30,56 @@ export default function Banner(props) {
     localStorage.removeItem("personName");
     localStorage.removeItem("personId");
     localStorage.removeItem("token");
-  }
+  }*/
+
   return (
     <Flex
       size="2xl"
       backgroundColor="blue.400"
-      paddingX="10px"
-      paddingY="15px"
       textColor="white"
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      w="100%"
+      mb={8}
+      p={8}
     >
       <Heading
         font="sans-serif"
-        pl={20}
+        pl={16}
         // color="#CB6CE6"
         // color="black"
       >
         Big At Heart
       </Heading>
-      {props.loggedIn ? <Nav /> : <Box />}
-      <Spacer />
-      {props.loggedIn ? (
-        <Text
-          as='b'
-          fontSize='md'
-          font='sans-serif'
-          pt='2'
-          pr='8'
-        >
-          Welcome, {localStorage.getItem("personName")}!
-        </Text> 
-      ) : ( 
-        <Box />
-      )}
-      {props.loggedIn ? (
-        <Button
-          onClick={handleLogout}
-          position="right"
-          color="white"
-          variant="outline"
-        >
-          Logout
-        </Button>
-      ) : (
-        <Box />
-      )}
+      {props.loggedIn ? <MenuToggle toggle={toggle} isOpen={isOpen} /> : <Box />}
+      {props.loggedIn ? <Nav toast={props.toast} loggedIn={props.loggedIn} isOpen={isOpen} /> : <Box />}
+      {/**props.loggedIn ? (
+          <Text
+            as='b'
+            fontSize='md'
+            font='sans-serif'
+            pt='2'
+            pr='8'
+          >
+            Welcome, {localStorage.getItem("personName")}!
+          </Text> 
+        ) : ( 
+          <Box />
+        )}
+        {props.loggedIn ? (
+          <Button
+            onClick={handleLogout}
+            position="right"
+            color="white"
+            variant="outline"
+          >
+            Logout
+          </Button>
+        ) : (
+          <Box />
+        )*/}
     </Flex>
   );
 }
