6edfb1c613184e662938593164a5a2a10d4a282e
Kb-tay <taykaiboon12@gmail.com>
2024-02-09 15:41:52 +0800

    QR code reader implement

--- frontend/src/components/main/nav.jsx
+++ frontend/src/components/main/nav.jsx
@@ -5,6 +5,9 @@ export default function Nav(props) {
   return (
     <Stack paddingLeft="30px" alignItems="end" direction={"row"}>
       <Box paddingX="5px" fontWeight="bold">
+        <Link to="./attendance">Attendance</Link>
+      </Box>
+      <Box paddingX="5px" fontWeight="bold">
         <Link to="./dashboard">Dashboard</Link>
       </Box>
       <Box paddingX="5px" fontWeight="bold">
